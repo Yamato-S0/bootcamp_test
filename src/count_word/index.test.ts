abcbac0dc83b8fa414c639f7589761d2e893aaa2
Yamato <shinohara559@gmail.com>
2023-07-17 18:07:56 +0900

    add tests

--- src/count_word/index.test.ts
+++ src/count_word/index.test.ts
@@ -1,2 +1,57 @@
 import {countWord} from "@/count_word";
 
+describe("countWord", () => {
+  test("should return -1 when text is empty", () => {
+    const text = "";
+    const targetWord = "foo";
+
+    const actual = countWord(text, targetWord);
+
+    expect(actual).toBe(-1);
+  });
+
+  test("should return -1 when targetWord is empty", () => {
+    const text = "foo";
+    const targetWord = "";
+
+    const actual = countWord(text, targetWord);
+
+    expect(actual).toBe(-1);
+  });
+
+  test("should return -1 when text and targetWord are empty", () => {
+    const text = "";
+    const targetWord = "";
+
+    const actual = countWord(text, targetWord);
+
+    expect(actual).toBe(-1);
+  });
+
+  test("should return 0 when text and targetWord are not matched", () => {
+    const text = "foo";
+    const targetWord = "bar";
+
+    const actual = countWord(text, targetWord);
+
+    expect(actual).toBe(0);
+  });
+
+  test("should return 1 when text and targetWord are matched", () => {
+    const text = "foo";
+    const targetWord = "foo";
+
+    const actual = countWord(text, targetWord);
+
+    expect(actual).toBe(1);
+  });
+
+  test("should return 2 when text and targetWord are matched", () => {
+    const text = "foo foo";
+    const targetWord = "foo";
+
+    const actual = countWord(text, targetWord);
+
+    expect(actual).toBe(2);
+  });
+});
