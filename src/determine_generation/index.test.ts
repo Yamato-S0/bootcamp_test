758dc7af2ee30aef85f97bff61b1fd2c8363f426
Yamato <shinohara559@gmail.com>
2023-07-17 17:52:46 +0900

    add test

--- src/determine_generation/index.test.ts
+++ src/determine_generation/index.test.ts
@@ -8,12 +8,26 @@ describe("determineGeneration", () => {
     test("Child: age = 10", () => {
       expect(determineGeneration(10)).toBe("Child");
     });
+    test("Young: age = 20", () => {
+      expect(determineGeneration(20)).toBe("Young");
+    });
+    test("Adult: age = 40", () => {
+      expect(determineGeneration(40)).toBe("Adult");
+    });
   });
 
   describe("boundary value test", () => {
+    test("Invalid age boundary: age = -1, age = 0", () => {
+      expect(determineGeneration(-1)).toBe("Invalid age");
+      expect(determineGeneration(0)).toBe("Invalid age");
+    });
     test("Child and Young boundary: age = 12, age = 13", () => {
       expect(determineGeneration(12)).toBe("Child");
       expect(determineGeneration(13)).toBe("Young");
+    });
+    test("Young and Adult boundary: age = 29, age = 30", () => {
+      expect(determineGeneration(29)).toBe("Young");
+      expect(determineGeneration(30)).toBe("Adult");
     });
   });
 });
