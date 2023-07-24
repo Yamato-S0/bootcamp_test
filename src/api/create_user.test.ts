9ac2434c4d3986eb1a3d94990fcea0b570d4483d
Yamato <shinohara559@gmail.com>
2023-07-24 16:38:44 +0900

    add

--- src/api/create_user.test.ts
+++ src/api/create_user.test.ts
@@ -8,4 +8,21 @@ describe("createUser", () => {
     const db = await databaseManager.getInstance();
     await resetDB(db);
   });
+
+  test("returns user on success", async () => {
+    const ret = await createUser("Test User", "Test Email");
+    expect(ret).not.toBeNull();
+    expect(ret!.name).toBe("Test User");
+    expect(ret!.email).toBe("Test Email");
+  });
+
+  test("returns null on empty name", async () => {
+    const ret = await createUser("", "Test Email");
+    expect(ret).toBeNull();
+  });
+
+  test("returns null on empty email", async () => {
+    const ret = await createUser("Test User", "");
+    expect(ret).toBeNull();
+  });
 });
