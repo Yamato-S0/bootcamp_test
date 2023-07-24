2e4577c9120bd42e83c2a143d2d6fe80231b62b3
Yamato <shinohara559@gmail.com>
2023-07-24 16:30:51 +0900

    add

--- src/api/get_user_name.test.ts
+++ src/api/get_user_name.test.ts
@@ -1,3 +1,34 @@
 import {getUserName} from "@/api/get_user_name";
 import {fetchUser} from "@/api/fetch_user";
 
+jest.mock("@/api/fetch_user", () => {
+  return {
+    fetchUser: jest.fn().mockImplementation((id: number) => {
+      if (id === 1) {
+        return Promise.resolve({name: "Mock User Name"});
+      } else {
+        return Promise.resolve(null);
+      }
+    }),
+  };
+});
+
+describe("getUserName", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("returns user name on fetchUser success", async () => {
+    const ret = await getUserName(1);
+    expect(ret).toBe("Mock User Name");
+    expect(fetchUser).toHaveBeenCalledWith(1);
+    expect(fetchUser).toBeCalledTimes(1);
+  });
+
+  test("returns null on fetchUser failure", async () => {
+    const ret = await getUserName(-1);
+    expect(ret).toBeNull();
+    expect(fetchUser).toHaveBeenCalledWith(-1);
+    expect(fetchUser).toBeCalledTimes(1);
+  });
+});
